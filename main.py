5d092d2f6d84763a9ea2133cba0240d5ee7a2936
Nicorufino <nicorufino14@gmail.com>
2025-06-09 18:59:07 -0300

    finished project

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+import sys
 from stats import count_words
 from stats import count_characters
+from stats import sort_char_dictionary
 
-def main():
-    book = get_book_text("/home/rufi/workspace/github.com/nicorufino/bookbot/books/frankenstein.txt")
+
+def main(args):
+    #Exit the program if the number of arguments is incorrect
+    if (len(args) != 2):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book = get_book_text(args[1])
     num_words = count_words(book)
-    print(f"{num_words} words found in the document")
-    print(count_characters(book))
+    sorted_results = sort_char_dictionary(count_characters(book))
+    print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for dictionary in sorted_results:
+        if (dictionary["char"].isalpha()):
+            print(f"{dictionary["char"]}: {dictionary["num"]}")
+    print("============= END ===============")
 
 def get_book_text(path: str):
     with open(path) as f:
         return f.read()
     
-main()
+main(sys.argv)
--- stats.py
+++ stats.py
@@ -13,3 +13,14 @@ def count_characters(text):
                 characters_dictionary[char] = 1
 
     return characters_dictionary
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_char_dictionary(dictionary):
+    keys = list(dictionary.keys())
+    complete_dictionary = []
+    for i in range(len(dictionary)):
+        complete_dictionary.append({"char": keys[i], "num": dictionary[keys[i]]})
+    complete_dictionary.sort(reverse=True, key=sort_on)
+    return complete_dictionary
